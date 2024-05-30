4451f4bbfcec08e0283c2ee2b6c1f9791e5f6215
shahinaz serour <you@example.com>
2024-05-30 18:21:34 +0300

    mainv2

--- lib/helpers/sql_helpers.dart
+++ lib/helpers/sql_helpers.dart
@@ -2,67 +2,56 @@ import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
-class SqlHelper{
+class SqlHelper {
   Database? db;
-  
-  Future <bool> createTables() async {
 
-    var batch =db!.batch();
-    
-    batch.execute(""" Create table  If not exists categories (
+  Future<bool> createTables() async {
+    try {
+      var batch = db!.batch();
+
+      batch.execute(""" Create table  If not exists categories (
         id integer primary key,
         name text,description text)
       """);
 
-    batch.execute(""" Create table  If not exists products (
+      batch.execute(""" Create table  If not exists products (
         id integer primary key,
         name text,description text,
         price double,stock integer,isAvailable boolean,
-        image blob,categoryId integer, )
+        image blob,categoryId integer )
       """);
-    batch.execute(""" Create table  If not exists clients (
+      batch.execute(""" Create table  If not exists clients (
         id integer primary key,
         name text,
         email text, phone text,
          address text)
       """);
-   
 
+      var result = await batch.commit();
 
-    var result = await batch.commit();
-    try {
-      
       print('Tables created successfully: $result');
       return true;
-    } catch(e)  {
+    } catch (e) {
       print('error in create  tables $e');
       return false;
-      
     }
   }
 
-    Future <void> initDb() async {
+  Future<void> initDb() async {
     try {
       if (kIsWeb) {
-        var factory= databaseFactoryFfiWeb;
+        var factory = databaseFactoryFfiWeb;
         db = await factory.openDatabase('pos.db');
         print('===========Db created');
       } else {
-    db = await openDatabase('pos.db',
-    version: 1,
-    onCreate: (db, version) => {
-    print('===========Db created')
-  },
-  );
+        db = await openDatabase(
+          'pos.db',
+          version: 1,
+          onCreate: (db, version) => {print('===========Db created')},
+        );
       }
-    }catch (e) {
+    } catch (e) {
       print('Error in create db:${e}');
     }
   }
-
-
-
-
-
-    }
-  
+}
